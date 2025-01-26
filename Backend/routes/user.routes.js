f3e813a754962de060ece611604aa1a7e69e88a0
Atul Paswan <atulpaswan324@gmail.com>
2025-01-26 16:23:31 +0530

    user profile page is done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-
 const userController = require('../controllers/user-controller');
+const authMiddleware = require('../middleware/auth.middleware');
 
 
 router.post('/register', [body('email').isEmail().withMessage('Invalid email'), body('fullname.firstname').isLength({min:3}).withMessage
@@ -12,6 +12,8 @@ router.post('/register', [body('email').isEmail().withMessage('Invalid email'), 
 router.post('/login', [body('email').isEmail().withMessage('Invalid email'),body('password').isLength({min:3}).withMessage('Password must be at least 6 character long')
 
 ], userController.loginUser); 
+
+router.get('/profile',authMiddleware.authUser, userController.getUserProfile)
 
 
 module.exports = router;
